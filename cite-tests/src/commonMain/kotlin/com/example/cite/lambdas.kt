9160da99ed1f2a127d836275be0df6f03a8390e6
Jake Wharton <jw@squareup.com>
2023-02-05 20:10:52 -0500

    Lambdas in type tests

--- cite-tests/src/commonMain/kotlin/com/example/cite/lambdas.kt
+++ cite-tests/src/commonMain/kotlin/com/example/cite/lambdas.kt
@@ -13,3 +13,15 @@ fun propertyReferenceFile(): () -> String = ::__FILE__
 fun propertyReferenceType(): () -> String = ::__TYPE__
 fun propertyReferenceMember(): () -> String = ::__MEMBER__
 fun propertyReferenceLine(): () -> Int = ::__LINE__
+
+object LambdaType {
+	fun lambdaType(): () -> String = { __TYPE__ }
+	fun lambdaFile(): () -> String = { __FILE__ }
+	fun lambdaMember(): () -> String = { __MEMBER__ }
+	fun lambdaLine(): () -> Int = { __LINE__ }
+
+	fun propertyReferenceFile(): () -> String = ::__FILE__
+	fun propertyReferenceType(): () -> String = ::__TYPE__
+	fun propertyReferenceMember(): () -> String = ::__MEMBER__
+	fun propertyReferenceLine(): () -> Int = ::__LINE__
+}
--- cite-tests/src/commonTest/kotlin/com/example/cite/LambdasTest.kt
+++ cite-tests/src/commonTest/kotlin/com/example/cite/LambdasTest.kt
@@ -12,11 +12,27 @@ class LambdasTest {
 		assertEquals(10, lambdaLine().invoke())
 	}
 
+	@Test fun lambdaInType() {
+		assertEquals("lambdas.kt", LambdaType.lambdaFile().invoke())
+		assertEquals("LambdaType", LambdaType.lambdaType().invoke())
+		// TODO should be "lambdaMember".
+		assertEquals("<anonymous>", LambdaType.lambdaMember().invoke())
+		assertEquals(21, LambdaType.lambdaLine().invoke())
+	}
+
 	@Ignore // TODO https://github.com/JakeWharton/cite/issues/7
 	@Test fun propertyReference() {
 		assertEquals("lambdas.kt", propertyReferenceFile().invoke())
 		assertEquals("AnObject", propertyReferenceType().invoke())
 		assertEquals("propertyReferenceMember", propertyReferenceMember().invoke())
 		assertEquals(16, propertyReferenceLine().invoke())
+	}
+
+	@Ignore // TODO https://github.com/JakeWharton/cite/issues/7
+	@Test fun propertyReferenceInType() {
+		assertEquals("lambdas.kt", LambdaType.propertyReferenceFile().invoke())
+		assertEquals("AnObject", LambdaType.propertyReferenceType().invoke())
+		assertEquals("propertyReferenceMember", LambdaType.propertyReferenceMember().invoke())
+		assertEquals(16, LambdaType.propertyReferenceLine().invoke())
 	}
 }
