a3ca4f7a56cccfed7c845e9d7cf950349ae45ca8
Jon Poulton <jpoulton@pm.me>
2024-11-20 15:33:27 +0000

    Update failure message

--- cite-gradle-plugin/src/main/kotlin/com/jakewharton/cite/plugin/gradle/CitePlugin.kt
+++ cite-gradle-plugin/src/main/kotlin/com/jakewharton/cite/plugin/gradle/CitePlugin.kt
@@ -41,7 +41,7 @@ public class CitePlugin : KotlinCompilerPluginSupportPlugin {
 		}
 		target.afterEvaluate {
 			check(applied) {
-				"The Cite plugin requires either the Kotlin multiplatform or JVM plugin"
+				"The Cite plugin requires either the Kotlin Multiplatform, JVM or Android plugin"
 			}
 		}
 	}
