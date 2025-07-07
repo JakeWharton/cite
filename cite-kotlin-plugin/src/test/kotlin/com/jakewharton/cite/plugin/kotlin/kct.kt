7b27d8b519c56541017003c1f0a52d89273e62a1
Jake Wharton <jw@squareup.com>
2025-07-06 22:46:08 -0400

    Test harness updates

--- cite-kotlin-plugin/src/test/kotlin/com/jakewharton/cite/plugin/kotlin/kct.kt
+++ cite-kotlin-plugin/src/test/kotlin/com/jakewharton/cite/plugin/kotlin/kct.kt
@@ -8,19 +8,12 @@ import com.tschuchort.compiletesting.JvmCompilationResult
 import com.tschuchort.compiletesting.KotlinCompilation
 import com.tschuchort.compiletesting.KotlinCompilation.ExitCode
 import com.tschuchort.compiletesting.SourceFile
-import org.jetbrains.kotlin.compiler.plugin.AbstractCliOption
-import org.jetbrains.kotlin.compiler.plugin.CommandLineProcessor
 
 fun jvmCompile(vararg files: SourceFile): Assert<JvmCompilationResult> {
 	return KotlinCompilation()
 		.apply {
 			sources = files.toList()
 			compilerPluginRegistrars = listOf(CiteCompilerPluginRegistrar())
-			// https://github.com/ZacSweers/kotlin-compile-testing/pull/124
-			commandLineProcessors = listOf(object : CommandLineProcessor {
-				override val pluginId get() = ""
-				override val pluginOptions get() = emptySet<AbstractCliOption>()
-			})
 			inheritClassPath = true
 		}
 		.compile()
