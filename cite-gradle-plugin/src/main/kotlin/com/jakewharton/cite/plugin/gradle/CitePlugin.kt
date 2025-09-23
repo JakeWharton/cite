53f20553f9f3bde4f8ead565d767b6a96c92ce61
Jake Wharton <github@jakewharton.com>
2025-09-23 12:11:52 -0400

    Better handle the absence of Kotlin on the buildscript classpath (#151)

--- cite-gradle-plugin/src/main/kotlin/com/jakewharton/cite/plugin/gradle/CitePlugin.kt
+++ cite-gradle-plugin/src/main/kotlin/com/jakewharton/cite/plugin/gradle/CitePlugin.kt
@@ -1,34 +1,36 @@
 package com.jakewharton.cite.plugin.gradle
 
 import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.SourceSet.MAIN_SOURCE_SET_NAME
 import org.gradle.api.tasks.SourceSet.TEST_SOURCE_SET_NAME
 import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinBaseApiPlugin
-import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation
-import org.jetbrains.kotlin.gradle.plugin.KotlinCompilerPluginSupportPlugin
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet.Companion.COMMON_MAIN_SOURCE_SET_NAME
-import org.jetbrains.kotlin.gradle.plugin.SubpluginArtifact
-import org.jetbrains.kotlin.gradle.plugin.SubpluginOption
 
-public class CitePlugin : KotlinCompilerPluginSupportPlugin {
+public class CitePlugin : Plugin<Project> {
 	override fun apply(target: Project) {
-		super.apply(target)
-
 		var applied = false
-		target.plugins.withId("org.jetbrains.kotlin.multiplatform") {
+		target.afterEvaluate {
+			check(applied) { "No suitable Kotlin configuration was found" }
+		}
+
+		target.pluginManager.withPlugin("org.jetbrains.kotlin.multiplatform") {
 			applied = true
+			target.pluginManager.apply(CiteKotlinPlugin::class.java)
+
 			val kotlin = target.extensions.getByType(KotlinMultiplatformExtension::class.java)
 			kotlin.sourceSets.getByName(COMMON_MAIN_SOURCE_SET_NAME).dependencies {
 				implementation(target.citeApiDependency())
 			}
 		}
-		target.plugins.withId("org.jetbrains.kotlin.jvm") {
+		target.pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
 			applied = true
+			target.pluginManager.apply(CiteKotlinPlugin::class.java)
+
 			val kotlin = target.extensions.getByType(KotlinJvmProjectExtension::class.java)
 			kotlin.sourceSets.getByName(MAIN_SOURCE_SET_NAME).dependencies {
 				compileOnly(target.citeApiDependency())
@@ -37,8 +39,10 @@ public class CitePlugin : KotlinCompilerPluginSupportPlugin {
 				compileOnly(target.citeApiDependency())
 			}
 		}
-		target.plugins.withId("org.jetbrains.kotlin.android") {
+		target.pluginManager.withPlugin("org.jetbrains.kotlin.android") {
 			applied = true
+			target.pluginManager.apply(CiteKotlinPlugin::class.java)
+
 			val kotlin = target.extensions.getByType(KotlinAndroidProjectExtension::class.java)
 			kotlin.sourceSets.getByName(MAIN_SOURCE_SET_NAME).dependencies {
 				compileOnly(target.citeApiDependency())
@@ -50,19 +54,15 @@ public class CitePlugin : KotlinCompilerPluginSupportPlugin {
 				compileOnly(target.citeApiDependency())
 			}
 		}
-		target.plugins.withId("com.android.base") {
+		target.pluginManager.withPlugin("com.android.base") {
 			if (target.plugins.hasPlugin(KotlinBaseApiPlugin::class.java)) {
 				applied = true
+				target.pluginManager.apply(CiteKotlinPlugin::class.java)
+
 				val android = target.extensions.getByType(CommonExtension::class.java)
 				android.sourceSets.configureEach { sourceSet ->
 					target.dependencies.add(sourceSet.compileOnlyConfigurationName, target.citeApiDependency())
 				}
-			}
-		}
-
-		target.afterEvaluate {
-			check(applied) {
-				"The Cite plugin requires either the Kotlin Multiplatform, JVM, or Android plugin"
 			}
 		}
 	}
@@ -79,21 +79,5 @@ public class CitePlugin : KotlinCompilerPluginSupportPlugin {
 		} else {
 			"com.jakewharton.cite:cite-api:$CiteVersion"
 		}
-	}
-
-	override fun isApplicable(kotlinCompilation: KotlinCompilation<*>): Boolean = true
-
-	override fun getCompilerPluginId(): String = "com.jakewharton.cite"
-
-	override fun getPluginArtifact(): SubpluginArtifact {
-		return SubpluginArtifact(
-			groupId = "com.jakewharton.cite",
-			artifactId = "cite-kotlin-plugin",
-			version = CiteVersion,
-		)
-	}
-
-	override fun applyToCompilation(kotlinCompilation: KotlinCompilation<*>): Provider<List<SubpluginOption>> {
-		return kotlinCompilation.target.project.provider { emptyList() }
 	}
 }
