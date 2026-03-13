29bc6d444630d478d5813f6b2cf787e9e86866dd
Jake Wharton <jw@jakewharton.com>
2026-03-13 10:54:42 -0400

    Track compiler plugin API deprecation (#205)

--- cite-kotlin-plugin/src/main/kotlin/com/jakewharton/cite/plugin/kotlin/CiteCompilerPluginRegistrar.kt
+++ cite-kotlin-plugin/src/main/kotlin/com/jakewharton/cite/plugin/kotlin/CiteCompilerPluginRegistrar.kt
@@ -1,9 +1,9 @@
 package com.jakewharton.cite.plugin.kotlin
 
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
-import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
+import org.jetbrains.kotlin.config.CommonConfigurationKeys
 import org.jetbrains.kotlin.config.CompilerConfiguration
 
 public class CiteCompilerPluginRegistrar : CompilerPluginRegistrar() {
@@ -12,7 +12,7 @@ public class CiteCompilerPluginRegistrar : CompilerPluginRegistrar() {
 
 	override fun ExtensionStorage.registerExtensions(configuration: CompilerConfiguration) {
 		val messageCollector = configuration.get(
-			CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY,
+			CommonConfigurationKeys.MESSAGE_COLLECTOR_KEY,
 			MessageCollector.NONE
 		)
 		IrGenerationExtension.registerExtension(CiteIrGenerationExtension(messageCollector))
